526082cdb0c92dbdb94b5940ef992105532b8f49
Mark Wardle <mark@wardle.org>
2016-07-12 23:00:48 +0100

    Add support for determining location of index when building from command-line.

--- rsterminology-server/src/main/java/com/eldrix/terminology/server/commands/CreateIndex.java
+++ rsterminology-server/src/main/java/com/eldrix/terminology/server/commands/CreateIndex.java
@@ -1,6 +1,9 @@
 package com.eldrix.terminology.server.commands;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.configuration.server.ServerRuntime;
@@ -37,13 +40,32 @@ public class CreateIndex extends CommandWithMetadata {
 
 	@Override
 	public CommandOutcome run(Cli cli) {
-		System.out.println("Building lucene index.... ");
-		ObjectContext context = cayenne.get().newContext();
-		try {
-			Search.getInstance().processAllDescriptions(context);
-			return CommandOutcome.succeeded();
-		} catch (IOException e) {
-			return CommandOutcome.failed(-1, e);
+		List<String> args = cli.standaloneArguments();
+		String filename = null;
+		if (args.size() == 1) {
+			filename = args.get(0);
 		}
+		else {
+			System.out.println("Enter filename:");
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				filename = bufferedReader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if (filename != null) {
+			System.out.println("Building lucene index at location: " + filename);
+			ObjectContext context = cayenne.get().newContext();
+			try {
+				Search.getInstance(filename).processAllDescriptions(context);
+				return CommandOutcome.succeeded();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return CommandOutcome.failed(-1, e);
+			}
+		}
+		return CommandOutcome.failed(1, "No file specified");
 	}
 }
