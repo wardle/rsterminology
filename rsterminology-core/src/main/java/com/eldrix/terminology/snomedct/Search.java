67ccdcd03cd4b044861eb12061fb3aa9c92c212b
Mark Wardle <mark@wardle.org>
2016-07-18 22:03:10 +0100

    Remove unused redundant superfluous dead code.

--- rsterminology-core/src/main/java/com/eldrix/terminology/snomedct/Search.java
+++ rsterminology-core/src/main/java/com/eldrix/terminology/snomedct/Search.java
@@ -246,17 +246,6 @@ public class Search {
 		return conceptsFromTopDocs(docs);
 	}
 
-	protected List<String> descriptionsFromTopDocs(TopDocs docs) throws CorruptIndexException, IOException {
-		ArrayList<String> descs = new ArrayList<String>(docs.totalHits);
-		ScoreDoc[] sds = docs.scoreDocs;
-		for (ScoreDoc sd : sds) {
-			Document doc = searcher().doc(sd.doc);
-			IndexableField term = doc.getField(FIELD_TERM);
-			descs.add(term.stringValue());
-		}
-		return Collections.unmodifiableList(descs);
-	}
-
 	protected List<Long> conceptsFromTopDocs(TopDocs docs) throws CorruptIndexException, IOException {
 		ArrayList<Long> concepts = new ArrayList<Long>(docs.totalHits);
 		ScoreDoc[] sds = docs.scoreDocs;
