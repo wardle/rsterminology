cd5e19a6ae838104d29ce4d39bf19b551dcdbbf7
David Hunnisett <david@inferiorplanet.co.uk>
2017-03-15 14:48:02 +0000

    instructions and a tidy up

--- docker/rsterminology/docker-entrypoint.sh
+++ docker/rsterminology/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+#wait for postgres to start
 while ! timeout 1 bash -c 'cat < /dev/null > /dev/tcp/pg/5432' >/dev/null 2>/dev/null; do sleep 0.1; done
 find SnomedCT_RF1Release_* -type f -exec java -jar rsterminology-server.jar --config config.yml --import-rf1 {} ';'
-#find SnomedCT_RF1Release_* -type f -exec java -jar rsterminology-server.jar --config config.yml --import-rf1 SnomedCT_RF1Release_GB1000000_20161001/Terminology/Content/sct1_Concepts_National_GB1000000_20161001.txt
+#import all the snowmed files
 
 java -jar rsterminology-server.jar --config config.yml  --build-parent-cache
 java -jar rsterminology-server.jar --config config.yml --build-index /var/rsdb/sct_lucene
